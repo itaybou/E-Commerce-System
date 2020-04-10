f9bb5fd90375aa4a0bc4db5b3e1d8422e22fd704
tomerbit <bitrant@post.bgu.ac.il>
2020-04-10 14:11:05 +0300

    Fix - move check of logged in user from store to storemanageMent

--- ECommerceSystem/ECommerceSystem/DomainLayer/StoresManagement/Store.cs
+++ ECommerceSystem/ECommerceSystem/DomainLayer/StoresManagement/Store.cs
@@ -33,15 +33,9 @@ namespace ECommerceSystem.DomainLayer.StoresManagement
 
 
         //@pre - logged in user have permission to add product
-        public bool addProductInv(string productName, Discount discount, PurchaseType purchaseType, double price, int quantity)
+        public bool addProductInv(string activeUserName, string productName, Discount discount, PurchaseType purchaseType, double price, int quantity)
         {
-            User loggedInUser = isLoggedInUserSubscribed();
-            if (loggedInUser == null) //The logged in user isn`t subscribed
-            {
-                return false;
-            }
-
-            if (!(_premmisions[loggedInUser.Name].canAddProduct()))
+            if (!(_premmisions[activeUserName].canAddProduct()))
             {
                 return false;
             }
@@ -50,26 +44,22 @@ namespace ECommerceSystem.DomainLayer.StoresManagement
         }
 
         //@pre - logged in user have permission to modify product
-        public bool addProduct(string productInvName, Discount discount, PurchaseType purchaseType, int quantity)
+        public bool addProduct(string loggedInUserName, string productInvName, Discount discount, PurchaseType purchaseType, int quantity)
         {
-            if (!isLoggedInUserCanMoidfy())
+            if (!isLoggedInUserCanMoidfy(loggedInUserName))
             {
                 return false;
             }
+
             else return _inventory.addProduct(productInvName, discount, purchaseType, quantity);
         }
 
 
         //@pre - logged in user have permission to delete product
-        public bool deleteProductInventory(string productInvName)
+        public bool deleteProductInventory(string loggedInUserName, string productInvName)
         {
-            User loggedInUser = isLoggedInUserSubscribed();
-            if (loggedInUser == null) //The logged in user isn`t subscribed
-            {
-                return false;
-            }
 
-            if (!(_premmisions[loggedInUser.Name].canDeleteProduct()))
+            if (!(_premmisions[loggedInUserName].canDeleteProduct()))
             {
                 return false;
             }
@@ -78,9 +68,9 @@ namespace ECommerceSystem.DomainLayer.StoresManagement
         }
 
         //@pre - logged in user have permission to modify product
-        public bool deleteProduct(string productInvName, int productID)
+        public bool deleteProduct(string loggedInUserName, string productInvName, int productID)
         {
-            if (!isLoggedInUserCanMoidfy())
+            if (!isLoggedInUserCanMoidfy(loggedInUserName))
             {
                 return false;
             }
@@ -88,25 +78,23 @@ namespace ECommerceSystem.DomainLayer.StoresManagement
         }
 
 
-        private bool isLoggedInUserCanMoidfy()
-        {
-            User loggedInUser = isLoggedInUserSubscribed();
-            if (loggedInUser == null) //The logged in user isn`t subscribed
-            {
-                return false;
-            }
+        //*********Modify Products*********
 
-            if (!(_premmisions[loggedInUser.Name].canModifyProduct()))
+
+        private bool isLoggedInUserCanMoidfy(string loggedInUserName)
+        {
+            if (!(_premmisions[loggedInUserName].canModifyProduct()))
             {
                 return false;
             }
-            return true;
+            else
+                return true;
         }
 
         //@pre - logged in user have permission to modify product
-        public bool modifyProductPrice(string productName, int newPrice)
+        public bool modifyProductPrice(string loggedInUserName, string productName, int newPrice)
         {
-            if (!isLoggedInUserCanMoidfy())
+            if (!isLoggedInUserCanMoidfy(loggedInUserName))
             {
                 return false;
             }
@@ -114,9 +102,9 @@ namespace ECommerceSystem.DomainLayer.StoresManagement
         }
 
         //@pre - logged in user have permission to modify product
-        public bool modifyProductDiscountType(string productInvName, int productID, Discount newDiscount)
+        public bool modifyProductDiscountType(string loggedInUserName, string productInvName, int productID, Discount newDiscount)
         {
-            if (!isLoggedInUserCanMoidfy())
+            if (!isLoggedInUserCanMoidfy(loggedInUserName))
             {
                 return false;
             }
@@ -124,9 +112,9 @@ namespace ECommerceSystem.DomainLayer.StoresManagement
         }
 
         //@pre - logged in user have permission to modify product
-        public bool modifyProductPurchaseType(string productInvName, int productID, PurchaseType purchaseType)
+        public bool modifyProductPurchaseType(string loggedInUserName, string productInvName, int productID, PurchaseType purchaseType)
         {
-            if (!isLoggedInUserCanMoidfy())
+            if (!isLoggedInUserCanMoidfy(loggedInUserName))
             {
                 return false;
             }
@@ -134,9 +122,9 @@ namespace ECommerceSystem.DomainLayer.StoresManagement
         }
 
         //@pre - logged in user have permission to modify product
-        public bool modifyProductQuantity(string productName, int productID, int newQuantity)
+        public bool modifyProductQuantity(string loggedInUserName, string productName, int productID, int newQuantity)
         {
-            if (!isLoggedInUserCanMoidfy())
+            if (!isLoggedInUserCanMoidfy(loggedInUserName))
             {
                 return false;
             }
@@ -144,9 +132,9 @@ namespace ECommerceSystem.DomainLayer.StoresManagement
         }
 
         //@pre - logged in user have permission to modify product
-        public bool modifyProductName(string newProductName, string oldProductName)
+        public bool modifyProductName(string loggedInUserName, string newProductName, string oldProductName)
         {
-            if (!isLoggedInUserCanMoidfy())
+            if (!isLoggedInUserCanMoidfy(loggedInUserName))
             {
                 return false;
             }
@@ -154,95 +142,70 @@ namespace ECommerceSystem.DomainLayer.StoresManagement
         }
 
 
-        private User isLoggedInUserSubscribed()
-        {
-            User loggedInUser = _userManagement.getLoggedInUser(); //sync
-            if (!loggedInUser.isSubscribed()) // sync
-            {
-                return null;
-            }
-            return loggedInUser;
-        }
-
 
 
         //*********Assign*********
 
-        public bool assignOwner(string userName)
+        public bool assignOwner(User loggedInUser, string newOwneruserName)
         {
-            User loggedInUser = isLoggedInUserSubscribed();
-            if (loggedInUser == null) //The logged in user isn`t subscribed
-            {
-                return false;
-            }
-
-            if (!_premmisions[loggedInUser.name].isOwner()) //Check that the assign is owner
-            {
-                return false;
-            }
-
-            if(_premmisions.ContainsKey(userName) && _premmisions[userName].isOwner() ) // The user of userName is already owner of this store
-            {
-                return false;
-            }
-
-            if (_premmisions.ContainsKey(userName))
-            {
-                _premmisions[userName].makeOwner();
-            }
-            else
-            {
-                Permissions per = Permissions.CreateOwner(loggedInUser);
-                if (per == null) return false;
-                _premmisions.Add(userName, per);
-            }
-            return true;
-        }
-
-        public bool assignManager(string userName)
-        {
-            User loggedInUser = isLoggedInUserSubscribed();
-            if (loggedInUser == null) //The logged in user isn`t subscribed
-            {
-                return false;
-            }
 
             if (!_premmisions[loggedInUser.Name].isOwner()) //Check that the assign is owner
             {
                 return false;
             }
 
-            if (_premmisions.ContainsKey(userName)) // The user of userName is already owner/manager of this store
+            if(_premmisions.ContainsKey(newOwneruserName) && _premmisions[newOwneruserName].isOwner() ) // The user of userName is already owner of this store
+            {
+                return false;
+            }
+
+            if (_premmisions.ContainsKey(newOwneruserName))
+            {
+                _premmisions[newOwneruserName].makeOwner();
+            }
+            else
+            {
+                Permissions per = Permissions.CreateOwner(loggedInUser);
+                if (per == null) return false;
+                _premmisions.Add(newOwneruserName, per);
+            }
+            return true;
+        }
+
+        public bool assignManager(User loggedInUser, string newManageruserName)
+        {
+
+            if (!_premmisions[loggedInUser.Name].isOwner()) //Check that the assign is owner
+            {
+                return false;
+            }
+
+            if (_premmisions.ContainsKey(newManageruserName)) // The user of userName is already owner/manager of this store
             {
                 return false;
             }
 
             Permissions per = Permissions.CreateManager(loggedInUser);
-            if (per == null) return false;
-            _premmisions.Add(userName, per);
+            if (per == null) return false; // loggedInUser isn`t subscribed
+            _premmisions.Add(newManageruserName, per);
             
             return true;
         }
 
-        public bool removeManager(string userName)
+        public bool removeManager(User loggedInUser, string managerUserName)
         {
-            User loggedInUser = isLoggedInUserSubscribed();
-            if (loggedInUser == null) //The logged in user isn`t subscribed
+
+            if (!_premmisions.ContainsKey(managerUserName) || _premmisions[managerUserName].isOwner()) // The user of userName isn`t manager of this store or he is owner of this store
             {
                 return false;
             }
 
-            if (!_premmisions.ContainsKey(userName) || _premmisions[userName].isOwner()) // The user of userName isn`t manager of this store or he is owner of this store
+            if (!_premmisions[managerUserName].AssignedBy.Name.Equals(loggedInUser.Name)) //check that the logged in the user who assigned userName
             {
                 return false;
             }
 
-            if (!_premmisions[userName].AssignedBy.Name.Equals(loggedInUser.Name)) //check that the logged in the user who assigned userName
-            {
-                return false;
-            }
-
-            _premmisions.Remove(userName);
+            _premmisions.Remove(managerUserName);
             return true;
         }
     }
--- ECommerceSystem/ECommerceSystem/DomainLayer/StoresManagement/StoresManagemet.cs
+++ ECommerceSystem/ECommerceSystem/DomainLayer/StoresManagement/StoresManagemet.cs
@@ -17,6 +17,20 @@ namespace ECommerceSystem.DomainLayer.StoresManagement
             this._userManagement = new UserManagement.UserManagement(); //TODO sync
             this._stores = new List<Store>();
         }
+        
+
+        // Return the user that logged in to the system if the user is subscribed
+        // If the user isn`t subscribed return null
+
+        private User isLoggedInUserSubscribed()
+        {
+            User loggedInUser = _userManagement.getLoggedInUser(); //sync
+            if (!loggedInUser.isSubscribed()) // sync
+            {
+                return null;
+            }
+            return loggedInUser;
+        }
 
 
         // Return null if the name isn`t exist
@@ -32,10 +46,11 @@ namespace ECommerceSystem.DomainLayer.StoresManagement
             return null; 
         }
 
+        //@pre - logged in user is subscribed
         public bool openStore(string name, DiscountPolicy discountPolicy, PurchasePolicy purchasePolicy)
         {
-            User loggedInUser = _userManagement.getLoggedInUser(); //sync
-            if (!loggedInUser.isSubscribed()) // sync
+            User loggedInUser = isLoggedInUserSubscribed();
+            if (loggedInUser == null) //The logged in user isn`t subscribed
             {
                 return false;
             }
@@ -45,77 +60,143 @@ namespace ECommerceSystem.DomainLayer.StoresManagement
                 return false;
             }
 
-            Store newStore = new Store(discountPolicy, purchasePolicy, loggedInUser.name, name); //sync - make user.name property
+            Store newStore = new Store(discountPolicy, purchasePolicy, loggedInUser.Name, name); //sync - make user.name property
             _userManagement.addOwnStore(newStore, loggedInUser);
             return true;
         }
 
         //*********Add, Delete, Modify Products*********
 
+        //@pre - logged in user is subscribed
         public bool addProductInv(string storeName, string productInvName, Discount discount, PurchaseType purchaseType, double price, int quantity)
         {
+            User loggedInUser = isLoggedInUserSubscribed();
+            if (loggedInUser == null) //The logged in user isn`t subscribed
+            {
+                return false;
+            }
+
             Store store = getStoreByName(storeName);
-            return store == null ? false : store.addProductInv(productInvName, discount, purchaseType, price, quantity);
+            return store == null ? false : store.addProductInv(loggedInUser.Name, productInvName, discount, purchaseType, price, quantity);
         }
 
+        //@pre - logged in user is subscribed
         public bool addProduct(string storeName, string productInvName, Discount discount, PurchaseType purchaseType, int quantity)
         {
+            User loggedInUser = isLoggedInUserSubscribed();
+            if (loggedInUser == null) //The logged in user isn`t subscribed
+            {
+                return false;
+            }
+
             Store store = getStoreByName(storeName);
-            return store == null ? false : store.addProduct(productInvName, discount, purchaseType, quantity);
+            return store == null ? false : store.addProduct(loggedInUser.Name, productInvName, discount, purchaseType, quantity);
         }
 
+        //@pre - logged in user is subscribed
         public bool deleteProductInventory(string storeName, string productInvName)
         {
+            User loggedInUser = isLoggedInUserSubscribed();
+            if (loggedInUser == null) //The logged in user isn`t subscribed
+            {
+                return false;
+            }
+
             Store store = getStoreByName(storeName);
-            return store == null ? false : store.deleteProductInventory(productInvName);
+            return store == null ? false : store.deleteProductInventory(loggedInUser.Name, productInvName);
         }
 
+        //@pre - logged in user is subscribed
         public bool deleteProduct(string storeName, string productInvName, int productID)
         {
+            User loggedInUser = isLoggedInUserSubscribed();
+            if (loggedInUser == null) //The logged in user isn`t subscribed
+            {
+                return false;
+            }
+
             Store store = getStoreByName(storeName);
-            return store == null ? false : store.deleteProduct(productInvName, productID);
+            return store == null ? false : store.deleteProduct(loggedInUser.Name, productInvName, productID);
         }
 
+        //@pre - logged in user is subscribed
         public bool modifyProductName(string storeName, string newProductName, string oldProductName)
         {
+            User loggedInUser = isLoggedInUserSubscribed();
+            if (loggedInUser == null) //The logged in user isn`t subscribed
+            {
+                return false;
+            }
+
             Store store = getStoreByName(storeName);
-            return store == null ? false : store.modifyProductName(newProductName, oldProductName);
+            return store == null ? false : store.modifyProductName(loggedInUser.Name, newProductName, oldProductName);
         }
 
+        //@pre - logged in user is subscribed
         public bool modifyProductPrice(string storeName, string productInvName, int newPrice)
         {
+            User loggedInUser = isLoggedInUserSubscribed();
+            if (loggedInUser == null) //The logged in user isn`t subscribed
+            {
+                return false;
+            }
             Store store = getStoreByName(storeName);
-            return store == null ? false : store.modifyProductPrice(productInvName, newPrice);
+            return store == null ? false : store.modifyProductPrice(loggedInUser.Name, productInvName, newPrice);
         }
 
+        //@pre - logged in user is subscribed
         public bool modifyProductQuantity(string storeName, string productInvName, int productID ,int newQuantity)
         {
+            User loggedInUser = isLoggedInUserSubscribed();
+            if (loggedInUser == null) //The logged in user isn`t subscribed
+            {
+                return false;
+            }
             Store store = getStoreByName(storeName);
-            return store == null ? false : store.modifyProductQuantity(productInvName, productID, newQuantity);
+            return store == null ? false : store.modifyProductQuantity(loggedInUser.Name, productInvName, productID, newQuantity);
         }
 
+        //@pre - logged in user is subscribed
         public bool modifyProductDiscountType(string storeName, string productInvName, int productID, Discount newDiscount)
         {
+            User loggedInUser = isLoggedInUserSubscribed();
+            if (loggedInUser == null) //The logged in user isn`t subscribed
+            {
+                return false;
+            }
             Store store = getStoreByName(storeName);
-            return store == null ? false : store.modifyProductDiscountType(productInvName, productID, newDiscount);
+            return store == null ? false : store.modifyProductDiscountType(loggedInUser.Name, productInvName, productID, newDiscount);
         }
 
+        //@pre - logged in user is subscribed
         public bool modifyProductPurchaseType(string storeName, string productInvName, int productID, PurchaseType purchaseType)
         {
+            User loggedInUser = isLoggedInUserSubscribed();
+            if (loggedInUser == null) //The logged in user isn`t subscribed
+            {
+                return false;
+            }
             Store store = getStoreByName(storeName);
-            return store == null ? false : store.modifyProductPurchaseType(productInvName, productID, purchaseType);
+            return store == null ? false : store.modifyProductPurchaseType(loggedInUser.Name, productInvName, productID, purchaseType);
         }
 
 
         //*********Assign*********
 
-        public bool assignOwner(string userName, string storeName)
+        //@pre - logged in user is subscribed
+        public bool assignOwner(string newOwneruserName, string storeName)
         {
+            User loggedInUser = isLoggedInUserSubscribed();
+            if (loggedInUser == null) //The logged in user isn`t subscribed
+            {
+                return false;
+            }
+
             Store store = getStoreByName(storeName);
-            bool isSuccess = store == null? false : store.assignOwner(userName);
+            bool isSuccess = store == null? false : store.assignOwner(loggedInUser, newOwneruserName);
             if (isSuccess)
             {
-                User assignedUser = _userManagement.getUserByName(userName);
+                User assignedUser = _userManagement.getUserByName(newOwneruserName);
                 _userManagement.addOwnStore(store, assignedUser);
                 return true;
             }
@@ -123,13 +204,21 @@ namespace ECommerceSystem.DomainLayer.StoresManagement
                 return false;
         }
 
-        public bool assignManager(string userName, string storeName)
+        //@pre - logged in user is subscribed
+        public bool assignManager(string newManageruserName, string storeName)
         {
+            User loggedInUser = isLoggedInUserSubscribed();
+            if (loggedInUser == null) //The logged in user isn`t subscribed
+            {
+                return false;
+            }
+
             Store store = getStoreByName(storeName);
-            bool isSuccess = store == null ? false : store.assignManager(userName);
+            bool isSuccess = store == null ? false : store.assignManager(loggedInUser, newManageruserName);
             if (isSuccess)
             {
-                User assignedUser = _userManagement.getUserByName(userName);
+                // Add the store to the list of the stores that the user manage
+                User assignedUser = _userManagement.getUserByName(newManageruserName);
                 _userManagement.addManagerStore(store, assignedUser);
                 return true;
             }
@@ -137,13 +226,21 @@ namespace ECommerceSystem.DomainLayer.StoresManagement
                 return false;
         }
 
-        public bool removeManager(string userName, string storeName)
+        //@pre - logged in user is subscribed
+        public bool removeManager(string managerUserName, string storeName)
         {
+            User loggedInUser = isLoggedInUserSubscribed();
+            if (loggedInUser == null) //The logged in user isn`t subscribed
+            {
+                return false;
+            }
+
             Store store = getStoreByName(storeName);
-            bool isSuccess = store == null ? false : store.removeManager(userName);
+            bool isSuccess = store == null ? false : store.removeManager(loggedInUser, managerUserName);
             if (isSuccess)
             {
-                User assignedUser = _userManagement.getUserByName(userName);
+                // Remove the store from the list of the stores that the user manage
+                User assignedUser = _userManagement.getUserByName(managerUserName);
                 _userManagement.removeManagerStore(store, assignedUser);
                 return true;
             }
