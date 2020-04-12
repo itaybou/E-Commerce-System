bd6bb70e68e0ec55811a301ca8e02f49f50c9563
itay <itaybou@post.bgu.ac.il>
2020-04-12 12:10:04 +0300

    Initiate unit test project

---
+++ code/test/ECommerceSystemUnitTests/UnitTest1.cs
@@ -0,0 +1,19 @@
+using NUnit.Framework;
+
+namespace Tests
+{
+    [TestFixture]
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [TestCase]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
