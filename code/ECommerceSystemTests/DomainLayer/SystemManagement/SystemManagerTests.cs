9a471c9369c8b5f1dc9cde6a53fdee2192bf03bb
itaybou <itaybou@post.bgu.ac.il>
2020-04-13 23:13:08 +0300

    Started testing purchase system

--- code/ECommerceSystemTests/DomainLayer/SystemManagement/SystemManagerTests.cs
+++ code/ECommerceSystemTests/DomainLayer/SystemManagement/SystemManagerTests.cs
@@ -5,12 +5,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ECommerceSystem.DomainLayer.StoresManagement;
 
 namespace ECommerceSystem.DomainLayer.SystemManagement.Tests
 {
     [TestFixture()]
     public class SystemManagerTests
     {
+        double totalPrice;
+        Dictionary<Product, int> allProducts;
+        Dictionary<Store, Dictionary<Product, int>> storeProducts;
+        IEnumerable<(Store, double)> storePayments;
+        string firstName; string lastName;
+        int id;
+        string creditCardNumber;
+        DateTime expirationCreditCard;
+        int CVV;
+        string address;
+
         [Test()]
         public void makePurchaseTest()
         {
