da0b4d70790e7306fb235415b0a8c0dbaf9330a9
itay <itaybou@post.bgu.ac.il>
2020-04-13 19:49:48 +0300

    Validation and User Shopping cart unit tests

---
+++ code/ECommerceSystemTests/DomainLayer/SystemManagement/SystemManagerTests.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+using ECommerceSystem.DomainLayer.SystemManagement;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceSystem.DomainLayer.SystemManagement.Tests
+{
+    [TestFixture()]
+    public class SystemManagerTests
+    {
+        [Test()]
+        public void makePurchaseTest()
+        {
+            Assert.Fail();
+        }
+
+        [Test()]
+        public void purchaseUserShoppingCartTest()
+        {
+            Assert.Fail();
+        }
+
+        [Test()]
+        public void purchaseProductsTest()
+        {
+            Assert.Fail();
+        }
+
+        [Test()]
+        public void purchaseProductTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
