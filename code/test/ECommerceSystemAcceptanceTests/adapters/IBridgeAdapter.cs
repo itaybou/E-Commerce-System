6995346007d4ac3d42f8a861ccdfd9bd477de223
itay <itaybou@post.bgu.ac.il>
2020-04-15 19:28:33 +0300

    Merge conflicts

---
+++ code/test/ECommerceSystemAcceptanceTests/adapters/IBridgeAdapter.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ECommerceSystemAcceptanceTests.adapters
+{
+    class IBridgeAdapter
+    {
+    }
+}
---
+++ code/test/ECommerceSystemAcceptanceTests/adapters/ProxyBridge.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ECommerceSystemAcceptanceTests.adapters
+{
+    class ProxyBridge : IBridgeAdapter
+    {
+        private IBridgeAdapter real;
+
+        internal IBridgeAdapter RealBridge { get => real; set => real = value; }
+    }
+}
